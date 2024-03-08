f8f04f1df2ae7f283f530799c25f3ace8be8e1b5
Tenteed <michall.kulla@gmail.com>
2024-03-08 23:36:10 +0100

    D08 - attempt at opening location of chosen application

--- homespaces/src/main.rs
+++ homespaces/src/main.rs
@@ -1,16 +1,19 @@
 use deunicode::deunicode;
-use std::process::Command;
+use std::io::{self, Write};
+use std::process::{Command, Stdio};
 
 fn main() {
+    let ps_command = r#"Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* |
+    Where-Object { $_.DisplayName -and $_.InstallLocation } |
+    Select-Object DisplayName, InstallLocation, @{Name="IsSystem"; Expression={if ($_.SystemComponent -eq 1) {"Yes"} else {"No"}}} |
+    Format-Table -AutoSize | Out-String -Width 4096"#;
+
     let output: std::process::Output = Command::new("powershell")
-        .args(&[
-            "-Command",
-            "Get-ItemProperty HKLM:\\Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\* | Select-Object DisplayName | Format-Table -AutoSize",
-        ])
+        .args(&["-Command", ps_command])
         .output()
         .expect("Failed to execute command");
 
-    let output_str: std::borrow::Cow<'_, str> = String::from_utf8_lossy(&output.stdout);
+    let output_str = String::from_utf8_lossy(&output.stdout);
 
     let mut result: Vec<String> = output_str
         .lines()
@@ -34,5 +37,35 @@ fn main() {
 
     for app in result {
         println!("{}", app);
+    }
+
+    let mut app_to_run = String::new();
+    println!("Enter app name");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut app_to_run).unwrap();
+
+    let lines = output_str.lines();
+    let mut found = false;
+
+    for line in lines {
+        if line.contains("DisplayName") && line.contains(&app_to_run) {
+            found = true;
+        } else if found && line.contains("InstallLocation") {
+            let install_location = line.split(":").nth(1).unwrap_or("").trim();
+            if !install_location.is_empty() {
+                println!("Attempting to open {}", install_location);
+                if let Ok(_) = Command::new("explorer")
+                    .arg(install_location)
+                    .stdout(Stdio::inherit())
+                    .stderr(Stdio::inherit())
+                    .spawn()
+                {
+                    println!("Opened");
+                } else {
+                    println!("Failed to open");
+                }
+                break;
+            }
+        }
     }
 }
